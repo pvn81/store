dcf938ad1ab3260a63c8026766408f725a6358e4
pvn81 <v_pechikin1981@mail.ru>
2022-10-30 17:29:51 +0200

    Описать сущность Book

---
+++ domain/Store/Book.cs
@@ -0,0 +1,12 @@
+﻿namespace Store;
+public class Book
+{
+    public int Id { get; }
+    public string Title { get; }
+    public Book(int id, string title)
+    {
+        this.Id = id;
+        this.Title = title;
+    }
+
+}
